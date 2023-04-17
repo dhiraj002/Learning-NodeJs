861d03930b9e671244ccb8dc398910a4a18deb50
Dhiraj Dhiman <dhirajswm15gmail.com>
2023-04-17 23:33:46 +0530

    added new routes

--- controllers/user.js
+++ controllers/user.js
@@ -1,28 +1,62 @@
 import { User } from "../models/user.js";
 
-export const getAllUsers=async (req, res) => {
-    const users = await User.find({});
-  
-    const keyword = req.query.keyword;
-    console.log(keyword);
-  
-    res.status(201).json({
-      success: true,
-      users,
-    });
-  }
+export const getAllUsers = async (req, res) => {
+  const users = await User.find({});
 
-  export const createNewUser=async (req, res) => {
-    const { name, email, password } = req.body;
-  
-    await User.create({
-      name,
-      email,
-      password,
-    });
-  
-    res.status(201).json({
-      success: true,
-      message: "Registered successfully",
-    });
-  }
+  const keyword = req.query.keyword;
+  console.log(keyword);
+
+  res.status(201).json({
+    success: true,
+    users,
+  });
+};
+
+export const createNewUser = async (req, res) => {
+  const { name, email, password } = req.body;
+
+  await User.create({
+    name,
+    email,
+    password,
+  });
+
+  res.status(201).json({
+    success: true,
+    message: "Registered successfully",
+  });
+};
+
+export const getUserById= async(req,res)=>{
+
+  const id = req.params.id;
+  const user = await User.findById(id);
+
+  res.json({
+    success:true,
+    user
+  })
+}
+
+export const updateUser=async(req,res)=>{
+  const id=req.params.id;
+  const user=await User.findById(id);
+  console.log(user);
+  res.json({
+    success:true,
+    message:"user Updated",
+  })
+}
+
+export const deleteUser = async (req, res) => {
+  const id = req.params.id;
+  const user = await User.findById(id);
+  console.log(user);
+
+  res.json({
+    success: true,
+    message: "user deleted",
+  });
+};
+
+
--- routes/user.js
+++ routes/user.js
@@ -1,11 +1,19 @@
 import express from "express";
 
-import { createNewUser, getAllUsers } from "../controllers/user.js";
+import {
+  createNewUser,
+  deleteUser,
+  getAllUsers,
+  getUserById,
+  updateUser,
+} from "../controllers/user.js";
 
 const router = express.Router();
 
-router.get("/all",getAllUsers);
+router.get("/all", getAllUsers);
 
-router.post("/new",createNewUser );
+router.post("/new", createNewUser);
+
+router.route("/userid/:id").put(updateUser).delete(deleteUser).get(getUserById);
 
 export default router;
